34c40fe1c9cf4c64412b656c2a8e206d6a42a402
venu <venugopaltekumudi@gmail.com>
2025-06-16 14:04:16 +0530

    Fix: allow Render domain in Vite server config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 8080,
+    allowedHosts: ["image-generator-8j32.onrender.com"], // 👈 Add your Render domain here
+  },
+});
